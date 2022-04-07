a3acff293d1ce967517e19a73b2ba137c27bf961
Bianca <biancataisep@gmail.com>
2022-04-07 14:20:57 -0500

    customized the form tag

--- static/js/app.js
+++ static/js/app.js
@@ -6,29 +6,29 @@ var tbody = d3.select("tbody");
 
 // build the table by creating a function called buildTable.
 function buildTable(data) {
-    // First, clear out any existing data
+    // First, clear     out any existing data
     tbody.html("");
 
-// Add the forEach Function:
-//  Next, loop through each object in the data.
-data.forEach((dataRow) => {
-    // Tell JavaScript to find the <tbody> tag within the HTML and add/append a table row ("tr").
-    let row = tbody.append("tr");
+    // Add the forEach Function:
+    //  Next, loop through each object in the data.
+    data.forEach((dataRow) => {
 
-    // add code to loop through each field in the dataRow argument
-    // tell avaScript to reference one object from the array of UFO sightings, with Object.values
-    // add (dataRow) as the argument
-    // add forEach((val) to specify that we want one object per row.
-    Object.values(dataRow).forEach((val) => {
-        // create a variable to append data to a table 
-        // append each value of the object to a cell in the table data tag <td>
-        let cell = row.append("td");
-         // add the values (When we chain .text(value) to the variable, we are extracting only the text of the value.)
-        cell.text(val);
-        }
-    );
-});
+        // Tell JavaScript to find the <tbody> tag within the HTML and add/append a table row ("tr").
+        let row = tbody.append("tr");
 
+        // add code to loop through each field in the dataRow argument
+        // tell avaScript to reference one object from the array of UFO sightings, with Object.values
+        // add (dataRow) as the argument
+        // add forEach((val) to specify that we want one object per row.
+        Object.values(dataRow).forEach((val) => {
+            // create a variable to append data to a table 
+            // append each value of the object to a cell in the table data tag <td>
+            let cell = row.append("td");
+             // add the values (When we chain .text(value) to the variable, we are extracting only the text of the value.)
+            cell.text(val);
+        });
+    });
+  }
 // create a function called "handleClick" to handle what to do after an input is given.
 
 function handleClick() {
@@ -44,7 +44,7 @@ function handleClick() {
         // Apply `filter` to the table data to only keep the
         // rows where the `datetime` value matches the filter value
         filteredData = filteredData.filter(row => row.datetime === date);
-    }
+    };
 
     // Rebuild the table using the filtered data 
     // NOTE@: If no date was entered, the filteredData will
@@ -57,8 +57,3 @@ d3.selectAll("#filter-btn").on("click", handleClick);
 
 // Build the table when the page loads
 buildTable(tableData);
-
-
-
-
-}
